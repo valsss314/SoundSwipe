d68f1db637d9b76c1531dccaa95e848ce0c6f3b5
jcheng2025 <81487855+jcheng2025@users.noreply.github.com>
2025-10-29 19:25:41 -0400

    stuff

---
+++ SoundSwipe/SoundSwipe/SoundSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSwipeApp.swift
+//  SoundSwipe
+//
+//  Created by Jonathan Cheng on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundSwipe/SoundSwipeTests/SoundSwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSwipeTests.swift
+//  SoundSwipeTests
+//
+//  Created by Jonathan Cheng on 10/29/25.
+//
+
+import Testing
+@testable import SoundSwipe
+
+struct SoundSwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
