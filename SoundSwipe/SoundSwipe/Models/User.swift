3e94d30c301ee4c0781bee21f5076de94100beaa
jcheng2025 <81487855+jcheng2025@users.noreply.github.com>
2025-11-05 18:55:05 -0500

    add user model

---
+++ SoundSwipe/SoundSwipe/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  SoundSwipe
+//
+//  Created by Jonathan Cheng on 11/5/25.
+//
+
+import Foundation
+
+class User : Identifiable {
+  
+  let id : UUID;
+  let username : String;
+  let email : String;
+  
+}
